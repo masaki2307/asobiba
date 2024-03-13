7ca33dd9a16e4335b460ce92b9964cbdf2e88e76
masaki2307 <149597889+masaki2307@users.noreply.github.com>
2024-03-13 12:48:11 +0000

    Add post/new

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -3,40 +3,42 @@ class Public::PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
     @post = Post.new
-    
+    @genres = Genre.all
   end
-  
+
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
+    @post.genre_id = params[:post][:genre_id]
     if @post.save
       redirect_to post_path(@post)
     else
       render :new
-    end  
+    end
   end
 
   def edit
   end
-  
+
   def update
-    
+
   end
-  
+
   def destroy
-    
+
   end
-  
+
   private
-  
+
   def post_params
-    
-    params.require(:post).permit(:introduction, :title, :image, :user_id)
-    
+
+    params.require(:post).permit(:introduction, :title, :image, :user_id, :genre_id)
+
   end
-  
+
 end
