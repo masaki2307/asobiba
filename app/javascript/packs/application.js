9766f7417b5ce5cf8471de60cfbc925f5c495243
masaki2307 <149597889+masaki2307@users.noreply.github.com>
2024-03-25 14:43:59 +0000

    add log in 1/2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,9 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "script.js"
 import 'jquery'
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application"; 
 import Raty from "raty.js"
 window.raty = function(elem,opt) {
     let raty =  new Raty(elem,opt)
