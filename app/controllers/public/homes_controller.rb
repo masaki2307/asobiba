25c87f017dd7536267d8fc43ad4731d7a7b9b924
masaki2307 <149597889+masaki2307@users.noreply.github.com>
2024-04-11 12:52:34 +0000

    fix

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
-    @reviews = Post.all.order(review: :desc).limit(3)
+    @reviews = Post.includes(:favorites).sort {|a,b| b.favorites.size <=> a.favorites.size}
     @posts = Post.all.order(created_at: :desc).limit(3)
     
   end
